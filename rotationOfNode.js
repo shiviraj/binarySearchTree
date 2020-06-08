ff2fd31e97f381aa22804bbfca7b2303854130dd
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-06-08 15:46:37 +0530

    rotation of tree left rotation and right rotation

--- rotationOfNode.js
+++ rotationOfNode.js
@@ -17,33 +17,37 @@ const findNode = (tree, value, parent = null) => {
   else return findNode(tree.right, value, tree);
 };
 
-const isLeafNode = (node) => {
-  return node.left == null && node.right == null;
-};
-
 const rightRotate = (tree, value) => {
-  const { node } = findNode(tree, value);
-  tree.left = node.right;
-  node.right = tree;
-  return node;
+  const { node, parent } = findNode(tree, value);
+  const grandParent = findNode(tree, parent.value).parent;
+  if (parent.left.value != node.value) return tree;
+  parent.left = node.right;
+  node.right = parent;
+  if (grandParent == null) return node;
+  if (grandParent.right && grandParent.right.value == parent.value)
+    grandParent.right = node;
+  else grandParent.left = node;
+  return tree;
 };
 
 const leftRotate = (tree, value) => {
-  const { node } = findNode(tree, value);
-  tree.right = node.left;
-  node.left = tree;
-  return node;
+  const { node, parent } = findNode(tree, value);
+  const grandParent = findNode(tree, parent.value).parent;
+  if (parent.right.value != node.value) return tree;
+  parent.right = node.right;
+  node.left = parent;
+  if (grandParent == null) return node;
+  if (grandParent.left && grandParent.left.value == parent.value)
+    grandParent.left = node;
+  else grandParent.right = node;
+  return tree;
 };
 
 const main = () => {
-  const a = [10, 5, 20, 1, 8];
+  const a = [10, 5, 15, 3, 7, 12, 18, 2, 4, 6, 8, 11, 14, 16, 20];
   let tree = a.reduce(insertIntoTree, null);
-  tree = rightRotate(tree, 5);
-  console.log(JSON.stringify(tree, null, 8));
-  const a1 = [10, 5, 20, 15, 25];
-  let tree1 = a1.reduce(insertIntoTree, null);
-  tree1 = leftRotate(tree1, 20);
-  console.log(JSON.stringify(tree1, null, 8));
+  tree = leftRotate(tree, 15);
+  console.log(JSON.stringify(tree, null, 2));
 };
 
 main();
